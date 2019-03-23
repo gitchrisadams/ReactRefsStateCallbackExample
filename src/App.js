d97c2f3d562d8628b401c30c0e0b859fb6750e08
Christopher Adams <chrismichaeladams@gmail.com>
2019-03-23 11:32:23 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class RefsForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      petValue: ''
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { pet } = this.form;
+
+    this.setState({
+      petValue: pet.value
+    }, () => console.log('state callback: ', this.state.petValue));
+
+    console.log('this.state.petValue right after we call set state', this.state.petValue);
+  }
+
+  handleChange = (e) => {
+
+    const { pet } = this.form;
+
+    this.setState({
+      petValueDynamic: pet.value
+    });
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form
+          onSubmit={this.handleSubmit}
+          ref={form => this.form = form}>
+          <label>
+            Cat
+            <input type="radio" value="cat" name="pet" onChange={this.handleChange}/>
+          </label>
+          <label>
+            Dog
+            <input type="radio" value="dog" name="pet" onChange={this.handleChange}/>
+          </label>
+          <label>
+            Alligator
+            <input type="radio" value="alligator" name="pet" onChange={this.handleChange}/>
+          </label>
+          <input className="submitbutton" type="submit" value="Submit"/>
+        </form>
+
+        <div className="display-preview-container">
+          <div className="display-preview">
+            Submitted: {this.state.petValue}
+          </div>
+
+          <div className="display-preview">
+            Dynamic: {this.state.petValueDynamic}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default RefsForm;
